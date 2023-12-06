c11dd1eedd4fbb7b1a534d8ec55f1e0c46e3bd7c
rlagosg <rlagosg@yahoo.com>
2023-12-06 13:24:31 -0600

    first commit

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,58 @@
+import 'package:go_router/go_router.dart';
+
+import '../../presentation/screens/screens.dart';
+
+
+// GoRouter configuration
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [    
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/buttons',
+      builder: (context, state) => const ButtonsScreen(),
+    ),
+    GoRoute(
+      path: '/cards',
+      builder: (context, state) => const CardsScreen(),
+    ),
+    GoRoute(
+      path: '/progress',
+      //name: ProgressScreen.name,
+      builder: (context, state) => const ProgressScreen(),
+    ),
+
+    GoRoute(
+      path: '/snackbars',
+      //name: SnackbarScreen.name,
+      builder: (context, state) => const SnackbarScreen(),
+    ),
+
+    GoRoute(
+      path: '/animated',
+      //name: AnimatedScreen.name,
+      builder: (context, state) => const AnimatedScreen(),
+    ),
+
+    GoRoute(
+      path: '/ui-controls',
+      //name: UiControlsScreen.name,
+      builder: (context, state) => const UiControlsScreen(),
+    ),
+
+    GoRoute(
+      path: '/tutorial',
+      //name: AppTutorialScreen.name,
+      builder: (context, state) => const AppTutorialScreen(),
+    ),
+
+    GoRoute(
+      path: '/infinite',
+      //name: InfiniteScrollScreen.name,
+      builder: (context, state) => const InfiniteScrollScreen(),
+    ),
+  ],
+);
---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(        
+        title: const Text('My App'),
+      ),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  //const _HomeView({ super.key,});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        //physics: const BouncingScrollPhysics(),
+        itemCount: appMenuItems.length,
+        itemBuilder: (contex, index){
+          final menuItem = appMenuItems[index];
+          return _CustomListTitle(menuItem: menuItem);
+        }
+      );
+  }
+}
+
+class _CustomListTitle extends StatelessWidget {
+
+  const _CustomListTitle({ required this.menuItem, });
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary,),
+      trailing: Icon( Icons.arrow_forward_ios_outlined, color: colors.primary),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: (){ 
+        context.push(menuItem.link);
+      },
+    );
+  }
+}
